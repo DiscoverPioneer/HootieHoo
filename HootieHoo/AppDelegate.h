eb6e49fbcb283e91508af49e30e77407bfc0a1e4
Phil <dplaxer25@aol.com>
2014-10-17 00:05:52 -0400

    Initial Commit

---
+++ HootieHoo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  HootieHoo
+//
+//  Created by Phil Scarfi on 10/17/14.
+//  Copyright (c) 2014 Pioneer Mobile Application. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ HootieHoo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HootieHoo
+//
+//  Created by Phil Scarfi on 10/17/14.
+//  Copyright (c) 2014 Pioneer Mobile Application. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
